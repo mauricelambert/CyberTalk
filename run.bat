e5934301432b7b28c9819eec9048dbac2fd16e07
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2025-11-23 14:55:20 +0100

    Create run.bat

---
+++ run.bat
@@ -0,0 +1,8 @@
+start http://mailbox.fr:8080/mailbox.html
+start http://csv.attackers.c2:8080/csv/list/
+start http://pixeltracking.attackers.c2:8080/stats
+start http://sylphora-dynamics:8080/welcome.html
+start http://data.breach.onion:8080/search.html
+start http://talks.local:8080/
+explorer /e,C:\Users\Cecile\Downloads
+python server.py
