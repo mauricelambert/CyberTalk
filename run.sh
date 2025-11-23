13dd52ab671c62d6581c818891595aac227e25a4
Maurice Lambert <50479118+mauricelambert@users.noreply.github.com>
2025-11-23 14:56:11 +0100

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,7 @@
+open http://mailbox.fr:8080/mailbox.html
+open http://csv.attackers.c2:8080/csv/list/
+open http://pixeltracking.attackers.c2:8080/stats
+open http://sylphora-dynamics:8080/welcome.html
+open http://data.breach.onion:8080/search.html
+open http://talks.local:8080/
+python3 server.py
